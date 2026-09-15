3014ebe23371a3d417dbb2d5627cc5f217770870
jyotiyadav6505 <jyotiyadav6505@gmail.com>
2026-09-15 17:36:16 +0530

    Add LeetCode solutions

---
+++ Maths/reverse_integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverse(self, x):
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        reverse = 0
+
+        while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x = x // 10
+
+        reverse = reverse * sign
+
+        if reverse < -2147483648 or reverse > 2147483647:
+            return 0
+
+        return reverse
